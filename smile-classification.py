524a909527638a65fb61091b3c2d0d533f7ef1db
stronglong <truongkechun@gmail.com>
2017-06-16 19:03:17 +0700

    update stuffs

--- smile-classification.py
+++ smile-classification.py
@@ -211,11 +211,9 @@ def accuracy(predictions, labels):
 
 
 batch_size = 16
-patch_size = 5
-depth = 16
-num_hidden = 64
-beta_regul = 1e-3
-drop_out = 0.5
+patch_size = 3
+# depth = 16
+# num_hidden = 64
 
 graph = tf.Graph()
 
@@ -227,7 +225,6 @@ with graph.as_default():
   tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
   tf_valid_dataset = tf.constant(valid_dataset)
   tf_test_dataset = tf.constant(test_dataset)
-  global_step = tf.Variable(0)
   
   # Variables
   layer1_1weights = tf.Variable(tf.truncated_normal(
@@ -236,39 +233,39 @@ with graph.as_default():
 
   layer1_2weights = tf.Variable(tf.truncated_normal(
       [3, 3, 32, 32], stddev=0.1))
-  layer1_2biases = tf.Variable(tf.constant(0.0, shape=[32]))
+  layer1_2biases = tf.Variable(tf.constant(1.0, shape=[32]))
   
   layer2_1weights = tf.Variable(tf.truncated_normal(
       [3, 3, 32, 64], stddev=0.1))
-  layer2_1biases = tf.Variable(tf.constant(0.0, shape=[64]))
+  layer2_1biases = tf.Variable(tf.constant(1.0, shape=[64]))
 
   layer2_2weights = tf.Variable(tf.truncated_normal(
       [3, 3, 64, 64], stddev=0.1))
-  layer2_2biases = tf.Variable(tf.constant(0.0, shape=[64]))
+  layer2_2biases = tf.Variable(tf.constant(1.0, shape=[64]))
 
   layer3_1weights = tf.Variable(tf.truncated_normal(
       [3, 3, 64, 128], stddev=0.1))
-  layer3_1biases = tf.Variable(tf.constant(0.0, shape=[128]))
+  layer3_1biases = tf.Variable(tf.constant(1.0, shape=[128]))
 
   layer3_2weights = tf.Variable(tf.truncated_normal(
       [3, 3, 128, 128], stddev=0.1))
-  layer3_2biases = tf.Variable(tf.constant(0.0, shape=[128]))
+  layer3_2biases = tf.Variable(tf.constant(1.0, shape=[128]))
 
   layer3_3weights = tf.Variable(tf.truncated_normal(
       [3, 3, 128, 128], stddev=0.1))
-  layer3_3biases = tf.Variable(tf.constant(0.0, shape=[128]))
+  layer3_3biases = tf.Variable(tf.constant(1.0, shape=[128]))
 
   # layer4_1weights = tf.Variable(tf.truncated_normal(
   #     [3, 3, 256, 512], stddev=0.1))
-  # layer4_1biases = tf.Variable(tf.constant(0.0, shape=[512]))
+  # layer4_1biases = tf.Variable(tf.constant(1.0, shape=[512]))
 
   # layer4_2weights = tf.Variable(tf.truncated_normal(
   #     [3, 3, 512, 512], stddev=0.1))
-  # layer4_2biases = tf.Variable(tf.constant(0.0, shape=[512]))
+  # layer4_2biases = tf.Variable(tf.constant(1.0, shape=[512]))
 
   # layer4_3weights = tf.Variable(tf.truncated_normal(
   #     [3, 3, 512, 512], stddev=0.1))
-  # layer4_3biases = tf.Variable(tf.constant(0.0, shape=[512]))
+  # layer4_3biases = tf.Variable(tf.constant(1.0, shape=[512]))
 
   # big_shape = image_size // 4 * image_size // 4 * image_size // 4 * image_size // 4 * 512
   big_shape = 8192
@@ -281,16 +278,16 @@ with graph.as_default():
   #   [4096, 4096], dtype=tf.float32, stddev=0.1))
   # fc2b = tf.Variable(tf.constant(1.0, shape=[4096], dtype=tf.float32))
 
-  fc3w = tf.Variable(tf.truncated_normal(
-      [4096, 1000], dtype=tf.float32, stddev=0.1))
-  fc3b = tf.Variable(tf.constant(1.0, shape=[1000], dtype=tf.float32))
+  # fc3w = tf.Variable(tf.truncated_normal(
+  #     [4096, 1000], dtype=tf.float32, stddev=0.1))
+  # fc3b = tf.Variable(tf.constant(1.0, shape=[1000], dtype=tf.float32))
   
   fc4w = tf.Variable(tf.truncated_normal(
-      [1000, 2], dtype=tf.float32, stddev=0.1))
+      [4096, 2], dtype=tf.float32, stddev=0.1))
   fc4b = tf.Variable(tf.constant(1.0, shape=[2], dtype=tf.float32))
   
   # Model.
-  def model(data, keep_prob):
+  def model(data):
     # conv1
     conv1_1 = tf.nn.conv2d(data, layer1_1weights, [1,1,1,1], padding='SAME')    
     bias1_1 = tf.nn.relu(conv1_1 + layer1_1biases)
@@ -336,6 +333,8 @@ with graph.as_default():
     # shape = int(np.prod(pool4.get_shape()[1:]))
     shape = int(np.prod(pool3.get_shape()[1:]))
 
+
+
     # fully-connected layer
     # fc1
     # fc1w = tf.Variable(tf.truncated_normal(
@@ -345,47 +344,47 @@ with graph.as_default():
     # fc1 = tf.nn.relu(tf.matmul(pool4_flat, fc1w) + fc1b)
     pool3_flat = tf.reshape(pool3, [-1, shape])    
     fc1 = tf.nn.relu(tf.matmul(pool3_flat, fc1w) + fc1b)
+    # drop1 = tf.nn.dropout(fc1, keep_prob)
 
     # fc2
     # fc2w = tf.Variable(tf.truncated_normal(
     #   [4096, 4096], dtype=tf.float32, stddev=0.1))
     # fc2b = tf.Variable(tf.constant(1.0, shape=[4096], dtype=tf.float32))
     # fc2 = tf.nn.relu(tf.matmul(fc1, fc2w) + fc2b)
+    # drop1 = tf.nn.dropout(fc2, keep_prob)
 
     # fc3
     # fc3w = tf.Variable(tf.truncated_normal(
     #   [4096, 1000], dtype=tf.float32, stddev=0.1))
     # fc3b = tf.Variable(tf.constant(1.0, shape=[1000], dtype=tf.float32))
     # fc3 = tf.nn.relu(tf.matmul(fc2, fc3w) + fc3b)
-    fc3 = tf.nn.relu(tf.matmul(fc1, fc3w) + fc3b)
-    drop = tf.nn.dropout(fc3, keep_prob)
+    # fc3 = tf.nn.relu(tf.matmul(drop1, fc3w) + fc3b)
+    # drop2 = tf.nn.dropout(fc3, keep_prob)
 
     # fc4
     # fc4w = tf.Variable(tf.truncated_normal(
     #   [1000, 2], dtype=tf.float32, stddev=0.1))
     # fc4b = tf.Variable(tf.constant(1.0, shape=[2], dtype=tf.float32))
-    return tf.matmul(fc3, fc4w) + fc4b
+    return tf.matmul(fc1, fc4w) + fc4b
   
   # Training computation.
-  logits = model(tf_train_dataset, drop_out)
+  logits = model(tf_train_dataset)
   loss = tf.reduce_mean(
-    tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=tf_train_labels))
+    tf.nn.softmax_cross_entropy_with_logits(labels=tf_train_labels, logits=logits))
     
   # Optimizer.
-  learning_rate = tf.train.exponential_decay(0.05, global_step, 1000, 0.85, staircase=True)
   optimizer = tf.train.GradientDescentOptimizer(0.05).minimize(loss)
   
   # Predictions for the training, validation, and test data.
   train_prediction = tf.nn.softmax(logits)
-  valid_prediction = tf.nn.softmax(model(tf_valid_dataset, 1.0))
-  test_prediction = tf.nn.softmax(model(tf_test_dataset, 1.0))
+  valid_prediction = tf.nn.softmax(model(tf_valid_dataset))
+  test_prediction = tf.nn.softmax(model(tf_test_dataset))
 
-num_steps = 1001
+num_steps = 20001
 
 with tf.Session(graph=graph) as session:
-  tf.global_variables_initializer().run()
+  tf.initialize_all_variables().run()
   print('Initialized')
-  
   for step in range(num_steps):
     offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
     batch_data = train_dataset[offset:(offset + batch_size), :, :, :]
@@ -398,5 +397,4 @@ with tf.Session(graph=graph) as session:
       print('Minibatch accuracy: %.1f%%' % accuracy(predictions, batch_labels))
       print('Validation accuracy: %.1f%%' % accuracy(
         valid_prediction.eval(), valid_labels))
-
   print('Test accuracy: %.1f%%' % accuracy(test_prediction.eval(), test_labels))
